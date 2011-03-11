945be7701917149dcb5394ba26b67507500e2738
Ben Godfrey <ben.godfrey@wonga.com>
2011-03-11 17:10:25 +0000

    Add package definition for python-software-properties

--- manifests/ppa.pp
+++ manifests/ppa.pp
@@ -5,6 +5,8 @@ define apt::ppa(
 ) {
 	include apt
 
+    package { "python-software-properties": }
+
 	exec { "/usr/bin/add-apt-repository ${name}":
 		require => Package["python-software-properties"],
 		# TODO: unless => 'check'
