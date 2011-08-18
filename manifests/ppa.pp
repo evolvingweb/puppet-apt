1af9a13c40c33e1ab8fb56f33cb9aa5e16cead0e
Robert Navarro <rnavarro@phiivo.com>
2011-08-18 02:37:54 -0300

    Added some crazy bash madness to check if the ppa is installed already. Otherwise the manifest tries to add it on every run!

--- manifests/ppa.pp
+++ manifests/ppa.pp
@@ -4,6 +4,10 @@ define apt::ppa() {
 
   Class['apt'] -> Apt::Ppa[$title]
 
+  Exec {
+    onlyif => "/usr/bin/test ! $(/bin/ls /etc/apt/sources.list.d | /bin/grep -v $(echo \"${title}\" | /usr/bin/gawk 'match(\$0, /^ppa:(.*)\/(.*)$/, vals) {printf \"%s-%s\", vals[1], vals[2]}'))",
+  }
+
   exec { "apt-update-${name}":
     command     => "/usr/bin/aptitude update",
     refreshonly => true,
