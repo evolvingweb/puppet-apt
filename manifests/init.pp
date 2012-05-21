6e9232a6279556920b88f3df70cf0a40435909ba
Zach Leslie <zach@puppetlabs.com>
2012-05-21 15:48:00 -0700

    align =

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,11 +20,11 @@
 # Sample Usage:
 #  class { 'apt': }
 class apt(
-  $always_apt_update = false,
-  $disable_keys = undef,
-  $proxy_host = false,
-  $proxy_port = '8080',
-  $purge_sources_list = false,
+  $always_apt_update    = false,
+  $disable_keys         = undef,
+  $proxy_host           = false,
+  $proxy_port           = '8080',
+  $purge_sources_list   = false,
   $purge_sources_list_d = false
 ) {
 
