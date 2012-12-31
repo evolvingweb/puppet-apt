5cdf186c45ca05d5ccecd74aa00172b7eff93d35
Chris Rutter <Chris@RutterMail.com>
2012-12-31 00:16:29 +0000

    fix minor comment typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,7 +11,7 @@
 #   purge_sources_list - Accepts true or false. Defaults to false If set to
 #     true, Puppet will purge all unmanaged entries from sources.list"
 #   purge_sources_list_d - Accepts true or false. Defaults to false. If set
-#     to false, Puppet will purge all unmanaged entries from sources.list.d
+#     to true, Puppet will purge all unmanaged entries from sources.list.d
 #
 # Actions:
 #
