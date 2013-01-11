eee5ff8e38b0661a32376aabb2f4b76a914ba67a
Michael Moll <kvedulv@kvedulv.de>
2013-01-12 00:21:39 +0100

    whitespace fixes

--- manifests/ppa.pp
+++ manifests/ppa.pp
@@ -16,12 +16,12 @@ define apt::ppa(
   $filename_without_dots    = regsubst($filename_without_slashes, '\.', '_', G)
   $filename_without_ppa     = regsubst($filename_without_dots, '^ppa:', '', G)
   $sources_list_d_filename  = "${filename_without_ppa}-${release}.list"
-  
+
   $package = $::lsbdistrelease ? {
-	  /^[1-9]\..*|1[01]\..*|12.04$/ => 'python-software-properties',
-	  default  => 'software-properties-common',
+    /^[1-9]\..*|1[01]\..*|12.04$/ => 'python-software-properties',
+    default  => 'software-properties-common',
   }
-  
+
   if ! defined(Package[$package]) {
     package { $package: }
   }
