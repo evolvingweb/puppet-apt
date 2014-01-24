0203f31dbd62e5ac3301892fe45bae45aab3cc88
Danny van der Meulen <dmeulen@ebay.com>
2014-01-24 11:32:41 +0100

    apt::ppa: Don't run when ppa source file exists

--- manifests/ppa.pp
+++ manifests/ppa.pp
@@ -5,14 +5,19 @@ define apt::ppa(
 ) {
     require apt
 
+    $ppa  = regsubst($name, '^(ppa:)(.*)$', '\2')
+    $ppa_apt_name = regsubst($ppa, '/', '-', 'G')
+    $ppa_list_file = "${ppa_apt_name}-${::lsbdistcodename}.list"
+
     exec { "apt-update-${name}":
-        command     => "/usr/bin/aptitude update",
+        command     => '/usr/bin/aptitude update',
         refreshonly => true,
     }
 
     exec { "add-apt-repository-${name}":
         command => "/usr/bin/add-apt-repository ${name}",
-        notify  => Exec["apt-update-${name}"],
+        creates => "/etc/apt/sources.list.d/${ppa_list_file}",
+        notify  => Exec["apt-update-${name}"];
     }
 }
 
