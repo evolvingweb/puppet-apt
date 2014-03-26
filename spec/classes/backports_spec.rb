ed52e513db21eca180c4ac80cce0cfe116549653
Daniele Sluijters <github@daenney.net>
2014-03-26 17:13:08 +0100

    backports: Allow setting a custom priority.
    The module used to always pin backports to a priority of 200. This
    default is still retained but is now configurable.
    
    Additionally the default is now an Integer, not a 'quoted Integer' and
    the tests have been updated to reflect this. This matters for future
    parser as it will now kick people if they pass in a stringified integer
    as priority.

--- spec/classes/backports_spec.rb
+++ spec/classes/backports_spec.rb
@@ -1,6 +1,36 @@
 require 'spec_helper'
 describe 'apt::backports', :type => :class do
 
+  describe 'when asigning a custom priority to backports' do
+    let :facts do
+      {
+        'lsbdistcodename' => 'Karmic',
+        'lsbdistid'       => 'Ubuntu'
+      }
+    end
+
+    context 'integer priority' do
+      let :params do { :pin_priority => 500 } end
+
+      it { should contain_apt__source('backports').with({
+          'location'   => 'http://old-releases.ubuntu.com/ubuntu',
+          'release'    => 'karmic-backports',
+          'repos'      => 'main universe multiverse restricted',
+          'key'        => '437D05B5',
+          'key_server' => 'pgp.mit.edu',
+          'pin'        => 500,
+        })
+      }
+    end
+
+    context 'invalid priority' do
+      let :params do { :pin_priority => 'banana' } end
+      it 'should fail' do
+        expect { subject }.to raise_error(/must be an integer/)
+      end
+    end
+  end
+
   describe 'when turning on backports for ubuntu karmic' do
 
     let :facts do
@@ -16,7 +46,7 @@ describe 'apt::backports', :type => :class do
         'repos'      => 'main universe multiverse restricted',
         'key'        => '437D05B5',
         'key_server' => 'pgp.mit.edu',
-        'pin'        => '200',
+        'pin'        => 200,
       })
     }
   end
@@ -36,7 +66,7 @@ describe 'apt::backports', :type => :class do
         'repos'      => 'main contrib non-free',
         'key'        => '46925553',
         'key_server' => 'pgp.mit.edu',
-        'pin'        => '200',
+        'pin'        => 200,
       })
     }
   end
@@ -64,7 +94,7 @@ describe 'apt::backports', :type => :class do
         'repos'      => 'main contrib non-free',
         'key'        => '46925553',
         'key_server' => 'pgp.mit.edu',
-        'pin'        => '200',
+        'pin'        => 200,
       })
     }
   end
