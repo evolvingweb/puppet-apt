442ca3087077434c9f96376b9b3e289966392839
Morgan Haskel <morgan@puppetlabs.com>
2014-08-12 16:10:36 -0400

    Don't hardcode /etc/puppet as the puppetpath.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -26,7 +26,7 @@ RSpec.configure do |c|
     # Install module and dependencies
     puppet_module_install(:source => proj_root, :module_name => 'apt')
     hosts.each do |host|
-      shell('/bin/touch /etc/puppet/hiera.yaml')
+      shell("/bin/touch #{default['puppetpath']}/hiera.yaml")
       shell('puppet module install puppetlabs-stdlib --version 2.2.1', { :acceptable_exit_codes => [0,1] })
     end
   end
