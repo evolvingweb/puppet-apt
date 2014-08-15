97556124daf5d49a755fd55e545b5023f057f49e
Hunter Haugen <hunter@puppetlabs.com>
2014-08-15 16:22:46 -0700

    Remove stderr from stdout
    Sometimes there are lib errors on platforms with malformed packages.
    This shouldn't cause the facts to completely fail.

--- lib/facter/apt_security_updates.rb
+++ lib/facter/apt_security_updates.rb
@@ -2,7 +2,7 @@ Facter.add("apt_security_updates") do
   confine :osfamily => 'Debian'
   setcode do
     if File.executable?("/usr/lib/update-notifier/apt-check")
-      updates = Facter::Util::Resolution.exec('/usr/lib/update-notifier/apt-check 2>&1')
+      updates = Facter::Util::Resolution.exec('/usr/lib/update-notifier/apt-check 2>/dev/null')
       Integer(updates.strip.split(';')[1])
     end
   end
