761a4d26c7b05f91d914033236d14a3557e6eab6
Daniele Sluijters <daenney@users.noreply.github.com>
2014-09-24 15:27:06 -0700

    Fix a test that broke with #326.

--- spec/defines/builddep_spec.rb
+++ spec/defines/builddep_spec.rb
@@ -5,7 +5,7 @@ describe 'apt::builddep', :type => :define do
   let(:title) { 'my_package' }
 
   describe "defaults" do
-    it { should contain_exec("apt-builddep-my_package").that_notifies('Exec[apt_update]').with({
+    it { should contain_exec("apt-builddep-my_package").that_requires('Exec[apt_update]').with({
         'command' => "/usr/bin/apt-get -y --force-yes build-dep my_package",
         'logoutput' => 'on_failure'
       })
