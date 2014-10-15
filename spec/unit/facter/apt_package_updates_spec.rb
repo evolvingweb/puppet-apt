d090ae4ebb9041bc46e161f698732a6bc9d28399
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-10-15 15:44:50 +0200

    Refactor facts to improve performance:
    * Add an apt_has_updates boolean fact
    * Make other facts depend on it

--- spec/unit/facter/apt_package_updates_spec.rb
+++ spec/unit/facter/apt_package_updates_spec.rb
@@ -4,26 +4,28 @@ describe 'apt_package_updates fact' do
   subject { Facter.fact(:apt_package_updates).value }
   after(:each) { Facter.clear }
 
-  describe 'on Debian based distro missing update-notifier-common' do
+  describe 'when apt has no updates' do
     before { 
-    Facter.fact(:osfamily).stubs(:value).returns 'Debian'
-    File.stubs(:executable?).returns false
-  }
-  it { should == nil }
+      Facter.fact(:apt_has_updates).stubs(:value).returns false
+    }
+    it { should be nil }
   end
 
-  describe 'on Debian based distro' do
+  describe 'when apt has updates' do
     before { 
-    Facter.fact(:osfamily).stubs(:value).returns 'Debian'
-    File.stubs(:executable?).returns true
-    Facter::Util::Resolution.stubs(:exec).returns "puppet-common\nlinux-generic\nlinux-image-generic"
-  }
-  it {
-    if Facter.version < '2.0.0'
-      should == 'puppet-common,linux-generic,linux-image-generic'
-    else
-      should == ['puppet-common', 'linux-generic', 'linux-image-generic']
-    end
-  }
+      Facter.fact(:osfamily).stubs(:value).returns 'Debian'
+      File.stubs(:executable?) # Stub all other calls
+      Facter::Util::Resolution.stubs(:exec) # Catch all other calls
+      File.expects(:executable?).with('/usr/lib/update-notifier/apt-check').returns true
+      Facter::Util::Resolution.expects(:exec).with('/usr/lib/update-notifier/apt-check 2>&1').returns "1;2"
+      Facter::Util::Resolution.expects(:exec).with('/usr/lib/update-notifier/apt-check -p 2>&1').returns "puppet-common\nlinux-generic\nlinux-image-generic"
+    }
+    it {
+      if Facter.version < '2.0.0'
+        should == 'puppet-common,linux-generic,linux-image-generic'
+      else
+        should == ['puppet-common', 'linux-generic', 'linux-image-generic']
+      end
+    }
   end
 end
--- spec/unit/facter/apt_security_updates_spec.rb
+++ spec/unit/facter/apt_security_updates_spec.rb
@@ -4,21 +4,22 @@ describe 'apt_security_updates fact' do
   subject { Facter.fact(:apt_security_updates).value }
   after(:each) { Facter.clear }
 
-  describe 'on Debian based distro missing update-notifier-common' do
+  describe 'when apt has no updates' do
     before { 
-    Facter.fact(:osfamily).stubs(:value).returns 'Debian'
-    File.stubs(:executable?).returns false
-  }
-  it { should == nil }
+      Facter.fact(:apt_has_updates).stubs(:value).returns false
+    }
+    it { should be nil }
   end
 
-  describe 'on Debian based distro' do
+  describe 'when apt has security updates' do
     before { 
-    Facter.fact(:osfamily).stubs(:value).returns 'Debian'
-    File.stubs(:executable?).returns true
-    Facter::Util::Resolution.stubs(:exec).returns '14;7'
-  }
-  it { should == 7 }
+      Facter.fact(:osfamily).stubs(:value).returns 'Debian'
+      File.stubs(:executable?) # Stub all other calls
+      Facter::Util::Resolution.stubs(:exec) # Catch all other calls
+      File.expects(:executable?).with('/usr/lib/update-notifier/apt-check').returns true
+      Facter::Util::Resolution.expects(:exec).with('/usr/lib/update-notifier/apt-check 2>&1').returns "14;7"
+    }
+    it { should == 7 }
   end
 
 end
