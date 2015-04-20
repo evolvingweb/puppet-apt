2802868f96b8c9c2e42b6342317c540bf818aad0
Morgan Haskel <morgan@puppetlabs.com>
2015-04-20 12:44:10 -0700

    Iterate through multiple keys

--- lib/puppet/provider/apt_key/apt_key.rb
+++ lib/puppet/provider/apt_key/apt_key.rb
@@ -141,7 +141,14 @@ Puppet::Type.type(:apt_key).provide(:apt_key) do
       if File.executable? command(:gpg)
         extracted_key = execute(["#{command(:gpg)} --with-fingerprint --with-colons #{file.path} | awk -F: '/^fpr:/ { print $10 }'"], :failonfail => false)
         extracted_key = extracted_key.chomp
-        if extracted_key != name
+
+        found_match = false
+        extracted_key.each_line do |line|
+          if line.chomp == name
+            found_match = true
+          end
+        end
+        if not found_match
           fail("The id in your manifest #{resource[:name]} and the fingerprint from content/source do not match. Please check there is not an error in the id or check the content/source is legitimate.")
         end
       else
