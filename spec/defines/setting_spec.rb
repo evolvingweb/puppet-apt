cdac6a906258c68f11381525e8965a144798cf29
Wilson McCoubrey <wilson@puppet.com>
2017-03-16 11:27:49 +0000

    [MODULES-4528] Replace Puppet.version.to_f with Puppet::Util::Package.versioncmp

--- spec/defines/setting_spec.rb
+++ spec/defines/setting_spec.rb
@@ -95,7 +95,7 @@ describe 'apt::setting' do
 
     context 'with priority=1.2' do
       let(:params) { default_params.merge({ :priority => 1.2 }) }
-      if Puppet.version.to_f >= 4.0 || ENV["FUTURE_PARSER"] == 'yes'
+      if Puppet::Util::Package.versioncmp(Puppet.version, '4.0') >= 0 || ENV["FUTURE_PARSER"] == 'yes'
         it { is_expected.to compile.and_raise_error(/input needs to be a String/) }
       else
         it { is_expected.to compile.and_raise_error(/priority must be an integer or a zero-padded integer/) }
