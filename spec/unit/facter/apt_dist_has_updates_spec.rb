ab5c977b5d66840a74df12a7e64d7dff144acd73
Will Meek <william.meek@puppet.com>
2017-11-07 16:21:55 +0000

    Add a facter fact for dist-upgrade
    This commit adds a facter fact for dist-upgrade, along with
    associated facter facts.

--- spec/unit/facter/apt_dist_has_updates_spec.rb
+++ spec/unit/facter/apt_dist_has_updates_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe 'apt_has_updates fact' do
-  subject { Facter.fact(:apt_has_updates).value }
+describe 'apt_has_dist_updates fact' do
+  subject { Facter.fact(:apt_has_dist_updates).value }
 
   after(:each) { Facter.clear }
 
@@ -27,11 +27,13 @@ describe 'apt_has_updates fact' do
       File.stubs(:executable?) # Stub all other calls
       Facter::Util::Resolution.stubs(:exec) # Catch all other calls
       File.expects(:executable?).with('/usr/bin/apt-get').returns true
-      apt_output = "Inst tzdata [2015f-0+deb8u1] (2015g-0+deb8u1 Debian:stable-updates [all])\n" \
-                   "Conf tzdata (2015g-0+deb8u1 Debian:stable-updates [all])\n" \
-                   "Inst unhide.rb [13-1.1] (22-2~bpo8+1 Debian Backports:jessie-backports [all])\n" \
-                   "Conf unhide.rb (22-2~bpo8+1 Debian Backports:jessie-backports [all])\n"
-      Facter::Util::Resolution.expects(:exec).with('/usr/bin/apt-get -s -o Debug::NoLocking=true upgrade 2>&1').returns apt_output
+      Facter::Util::Resolution.expects(:exec).with('/usr/bin/apt-get -s -o Debug::NoLocking=true upgrade 2>&1').returns 'test'
+      File.expects(:executable?).with('/usr/bin/apt-get').returns true
+      apt_output = "Inst extremetuxracer [2015f-0+deb8u1] (2015g-0+deb8u1 Debian:stable-updates [all])\n" \
+                   "Conf extremetuxracer (2015g-0+deb8u1 Debian:stable-updates [all])\n" \
+                   "Inst planet.rb [13-1.1] (22-2~bpo8+1 Debian Backports:jessie-backports [all])\n" \
+                   "Conf planet.rb (22-2~bpo8+1 Debian Backports:jessie-backports [all])\n"
+      Facter::Util::Resolution.expects(:exec).with('/usr/bin/apt-get -s -o Debug::NoLocking=true dist-upgrade 2>&1').returns apt_output
     end
     it { is_expected.to be true }
   end
