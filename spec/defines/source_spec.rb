7b98b13df19ef7a073761ed7ba1521dd42012d4f
Eimhin Laverty <eimhin.laverty@puppet.com>
2018-10-22 14:33:04 +0100

    Removed duplicated test

--- spec/defines/source_spec.rb
+++ spec/defines/source_spec.rb
@@ -148,39 +148,8 @@ describe 'apt::source' do
                                                                                                                                                     source: 'http://apt.puppetlabs.com/pubkey.gpg')
       }
     end
-
-    context 'with simple key' do
-      let :params do
-        {
-          comment: 'foo',
-          location: 'http://debian.mirror.iweb.ca/debian/',
-          release: 'sid',
-          repos: 'testing',
-          key: GPG_KEY_ID,
-          pin: '10',
-          architecture: 'x86_64',
-          allow_unsigned: true,
-        }
-      end
-
-      it {
-        is_expected.to contain_apt__setting('list-my_source').with(ensure: 'present').with_content(%r{# foo\ndeb \[arch=x86_64 trusted=yes\] http://debian.mirror.iweb.ca/debian/ sid testing\n})
-                                                             .without_content(%r{deb-src})
-      }
-
-      it {
-        is_expected.to contain_apt__pin('my_source').that_comes_before('Apt::Setting[list-my_source]').with(ensure: 'present',
-                                                                                                            priority: '10',
-                                                                                                            origin: 'debian.mirror.iweb.ca')
-      }
-
-      it {
-        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source my_source").that_comes_before('Apt::Setting[list-my_source]').with(ensure: 'present',
-                                                                                                                                                    id: GPG_KEY_ID)
-      }
-    end
   end
-
+  
   context 'with allow_unsigned true' do
     let :facts do
       {
