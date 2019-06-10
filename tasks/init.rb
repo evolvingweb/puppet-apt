44f8af71695bc2be27aedcf742b99d29c144eb71
Erick Banks <erick@puppetlabs.com>
2019-06-10 13:32:31 +0100

    pdksync_heads/master-0-g7827fc2

--- tasks/init.rb
+++ tasks/init.rb
@@ -7,7 +7,7 @@ def apt_get(action)
   cmd = ['apt-get', action]
   cmd << '-y' if ['upgrade', 'dist-upgrade', 'autoremove'].include?(action)
   stdout, stderr, status = Open3.capture3(*cmd)
-  raise Puppet::Error, stderr if status != 0 # rubocop:disable GetText/DecorateFunctionMessage
+  raise Puppet::Error, stderr if status != 0
   { status: stdout.strip }
 end
 
